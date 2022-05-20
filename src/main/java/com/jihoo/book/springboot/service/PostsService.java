84852a5800972ec393b392fa36e657d65f396b83
wlgn8648 <wlgn8648@gmail.com>
2022-05-20 15:58:31 +0900

    20220520

--- src/main/java/com/jihoo/book/springboot/service/PostsService.java
+++ src/main/java/com/jihoo/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.jihoo.book.springboot.service;
 
 import com.jihoo.book.springboot.domain.posts.Posts;
 import com.jihoo.book.springboot.domain.posts.PostsRepository;
+import com.jihoo.book.springboot.web.dto.PostsListResponseDto;
 import com.jihoo.book.springboot.web.dto.PostsResponseDto;
 import com.jihoo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jihoo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //생성자로 Bean 객체를 주입받는 방법이 Autowired와 같은데, 이 때 생성자가 여기서 해결됨
 @Service
@@ -30,5 +34,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
